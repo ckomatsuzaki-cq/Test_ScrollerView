ea9281681b024fc51002332861fb84018c2ed43f
ckomatsuzaki-cq <c.komatsuzaki@caraquri.com>
2021-05-10 18:24:15 +0900

    ViewControllerでの画面表示処理

--- Test_ScrollView/ViewController.swift
+++ Test_ScrollView/ViewController.swift
@@ -11,25 +11,67 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var layoutLists = [LayoutList]()
+    let datePicker = UIDatePicker()
+    let toolbar = UIToolbar()
+    let dateFormatter = DateFormatter()
+    
+    struct CellData {
+        var title: String
+    }
+    
+    // データを作る
+    var cells: [CellData] = [
+        CellData(title: "TextFieldCell"),
+        CellData(title: "DatePickerCell"),
+        CellData(title: "ButtonCell")
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "TextFieldCell")
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "DatePickerCell")
+        self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "ButtonCell")
+        
+        createDatePicker()
     }
     
     // 表示するセルの数
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return self.cells.count
     }
     
-    // 表示するセルを返す
+    // 各セルを生成して返却
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        // 表示するセルを登録(先程命名した"Cell")
-        let layout = layoutLists[indexPath.section].layouts[indexPath.row]
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: layout.reuseIdentifier, for: indexPath)
+        let identifier = cells[indexPath.row].title
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        // セルの枠線
+        cell.layer.borderColor = UIColor.gray.cgColor
+        cell.layer.borderWidth = 1
         
-        cell.backgroundColor = .gray  // セルの色
+        setSubView(cell: cell, identifier: identifier)
+        
         return cell
+        
+    }
+    
+    func setSubView(cell: UICollectionViewCell, identifier: String) {
+        switch identifier {
+        case "TextFieldCell":
+            let textField = setUpTextField(textField: UITextField())
+            cell.contentView.addSubview(textField)
+        case "DatePickerCell":
+            let textField = setUpTextField(textField: UITextField())
+            textField.inputView = datePicker
+            textField.inputAccessoryView = toolbar
+            textField.text = dateFormatter.string(from: datePicker.date)
+            cell.contentView.addSubview(textField)
+        case "ButtonCell":
+            let button = setUpButton(button: UIButton())
+            cell.contentView.addSubview(button)
+        default:
+            return
+        }
     }
     
     // UICollectionViewの外周余白
@@ -43,12 +85,56 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     // 行の最小余白
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return 300
     }
     // 列の最小余白
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 1
     }
     
+    func setUpTextField(textField: UITextField) -> UITextField{
+        textField.frame = CGRect(x: 0, y: 32, width: 1000, height: 32)
+        textField.placeholder = "ここに入力"
+        textField.keyboardType = .default
+        textField.borderStyle = .roundedRect
+        textField.returnKeyType = .done
+        textField.clearButtonMode = .always
+        
+        return textField
+    }
     
+    func setUpButton(button: UIButton) -> UIButton{
+        button.frame = CGRect(x: 450, y: 32, width: 120, height: 40)
+        button.setTitle("Submit", for: UIControl.State.normal)
+        button.backgroundColor = UIColor.blue
+        
+        return button
+    }
+    
+    func createDatePicker(){
+        
+        datePicker.datePickerMode = .date
+        datePicker.locale = NSLocale(localeIdentifier: "ja_JP") as Locale
+        datePicker.preferredDatePickerStyle = .wheels
+        
+        
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneClicked))
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        toolbar.setItems([flexibleSpace, doneButton], animated: true)
+    }
+    
+    @objc func doneClicked(){
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale    = NSLocale(localeIdentifier: "ja_JP") as Locale?
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func tappedScreen(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
